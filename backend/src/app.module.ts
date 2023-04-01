d1f61f47a79cddd4f5c1597fa8f931ae3d5c7ba2
tusharshaha <tusharshaha128@gmail.com>
2023-04-01 22:21:22 +0600

    initalized auth resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xl13.mongodb.net/web_market?retryWrites=true&w=majority`,
+    ),
+    AuthModule,
+  ],
 })
 export class AppModule {}
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return `I am signup`;
+  }
+  signin() {
+    return 'I am signin';
+  }
+}
