1078fa9a1ad6b7f8b48e913a457ebc4c6c1c43cf
Tushar Kumar Shaha <86643536+tusharshaha@users.noreply.github.com>
2023-05-18 14:51:57 +0000

    updated global css and hero section

--- frontend/components/Home/HeroSection.tsx
+++ frontend/components/Home/HeroSection.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
 const HeroSection: React.FC = () => {
     return (
         <div className="hero-bg">
-            <div className='container mx-auto'>
-                <h1>hello world</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, architecto!</p>
+            <div className='container mx-auto pt-[80px] pb-[210px] text-white grid grid-cols-2'>
+                <div className='flex items-center px-6'>
+                    <div>
+                        <h1>The Manage Web Development Services</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, architecto!</p>
+                    </div>
+                </div>
+                <div>
+                    <Image src='/images/hero-img.png' height={650} width={650} alt="hero img" />
+                </div>
             </div>
         </div>
     );
