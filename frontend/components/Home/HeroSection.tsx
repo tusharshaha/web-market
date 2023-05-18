916586d0d54dd46b1fd731e365574e33a71478ce
Tushar Kumar Shaha <86643536+tusharshaha@users.noreply.github.com>
2023-05-18 17:21:32 +0000

    added button on hero section

--- frontend/components/Home/HeroSection.tsx
+++ frontend/components/Home/HeroSection.tsx
@@ -4,11 +4,15 @@ import React from 'react';
 const HeroSection: React.FC = () => {
     return (
         <div className="hero-bg">
-            <div className='container mx-auto px-10 pt-[80px] pb-[210px] text-white grid grid-cols-2'>
-                <div className='flex items-center px-6'>
+            <div className='cus-container pt-[80px] pb-[210px] text-white grid grid-cols-2'>
+                <div className='flex items-center px-6 capitalize'>
                     <div>
                         <h1 className='font-bold mb-4'>The Manage Web Development Services</h1>
-                        <p className='font-medium pr-14'>Touch the success! Domain and Secure Web Hosting from <span className='text-error'>$4.99 per month</span></p>
+                        <p className='font-medium '>Build your dream website with our <span className='text-error'>web development</span> services. We offer a wide range of services to help you create a website that meets your needs.</p>
+                        <div className='flex mt-8 font-semibold'>
+                            <button className='me-8 hero-btn'>View Jobs</button>
+                            <button className='hero-btn2'>Hire Developer</button>
+                        </div>
                     </div>
                 </div>
                 <div className='mr-[-50px]'>
