e8500629013fde16309d5aff39e22fe752e4018b
Tushar Kumar Shaha <86643536+tusharshaha@users.noreply.github.com>
2023-05-19 03:03:25 +0000

    updated nav manu hover animation

--- frontend/components/Shared/NavBar.tsx
+++ frontend/components/Shared/NavBar.tsx
@@ -16,18 +16,18 @@ const NavBar: React.FC = () => {
         return () => window.removeEventListener('scroll', changeColor);
     }, [])
     return (
-        <div className={`navbar ${color ? "bg-blue-800": ''} border-b-2 border-blue-800 bg-white-200 text-white font-semibold fixed top-0 transition duration-300`}>
+        <div className={`navbar ${color ? "bg-blue-800" : ''} border-b-2 border-blue-800 bg-white-200 text-white font-semibold fixed top-0 transition duration-300`}>
             <div className="container mx-auto px-10">
                 <div className="flex-1">
                     <Link href="/" className="btn btn-ghost normal-case text-xl">daisyUI</Link>
                 </div>
                 <div className="flex-none">
                     <ul className="flex gap-2">
-                        <li><a>Find Jobs</a></li>
-                        <li><a>Hire Developer</a></li>
-                        <li><a>Post Jobs</a></li>
-                        <li><a>Login</a></li>
-                        <li><a>Sign Up</a></li>
+                        <li><Link className="transition duration-300 hover:text-error" href="">Find Jobs</Link></li>
+                        <li><Link className="transition duration-300 hover:text-error" href="">Hire Developer</Link></li>
+                        <li><Link className="transition duration-300 hover:text-error" href="">Post Jobs</Link></li>
+                        <li><Link className="transition duration-300 hover:text-error" href="">Login</Link></li>
+                        <li><Link className="transition duration-300 hover:text-error" href="">Sign Up</Link></li>
                     </ul>
                 </div>
             </div>
