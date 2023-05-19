4a63694b6820fd9426955ea549a910c6b7fcb6b4
Tushar Kumar Shaha <86643536+tusharshaha@users.noreply.github.com>
2023-05-19 05:20:53 +0000

    added pseudo class on feature

--- frontend/components/Home/Features.tsx
+++ frontend/components/Home/Features.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Features: React.FC = () => {
@@ -10,7 +11,7 @@ const Features: React.FC = () => {
         </div>
         <div className='ml-20'>
           <p className='text-slate-500'>Focus on your business and avoid all the web development hassles. Our services guarantee unmatched performance, reliability, and choice with 24/7 support that acts as your extended team.</p>
-          <a className="mt-4 text-blue-700 font-semibold after:content-['*'] after:ml-0.5 after:text-red-500 after:h-1 after:w-[50px] after:bg-blue-700 block">Show all Features</a>
+          <Link href='' className="mt-4 text-blue-700 font-semibold mark">Show all Features</Link>
         </div>
       </div>
     </div>
