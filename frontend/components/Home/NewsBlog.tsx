5b59184984b0896f3e9e7594a12d766a049ba0ce
tusharshaha <tusharshaha128@gmail.com>
2023-05-22 14:33:11 +0600

    crated title for news and blogs

--- frontend/components/Home/NewsBlog.tsx
+++ frontend/components/Home/NewsBlog.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 const NewsBlog = () => {
   return (
     <div className='cus-container my-28'>
-      <div className='w-50 text-center'>
+      <div className='text-center'>
         <h2 className=''>Our Latest News & Blog</h2>
-        <p className='text-slate-500'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit alias, qui facilis itaque pariatur voluptatibus accusantium sequi ullam quisquam culpa.</p>
+        <p className='text-slate-500'>Stay Informed and Explore Our Latest Insights and Articles on Web Development</p>
       </div>
     </div>
   );
