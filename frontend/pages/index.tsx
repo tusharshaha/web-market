505f34a7094e1ef734ae196c3d618b030211ccea
tusharshaha <tusharshaha128@gmail.com>
2023-05-25 12:16:45 +0600

    delted work process section

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,7 +5,6 @@ import Features from "@/components/Home/Features";
 import WhyChooseUs from "@/components/Home/WhyChooseUs";
 import QustionSection from "@/components/Home/QustionSection";
 import NewsBlog from "@/components/Home/NewsBlog";
-import WorkProcess from "@/components/Home/WorkProcess";
 
 const Home: React.FC = () => {
 
@@ -13,7 +12,6 @@ const Home: React.FC = () => {
     <Layout>
         <HeroSection></HeroSection>
         <Features></Features>
-        <WorkProcess></WorkProcess>
         <WhyChooseUs></WhyChooseUs>
         <NewsBlog></NewsBlog>
         <QustionSection></QustionSection>
