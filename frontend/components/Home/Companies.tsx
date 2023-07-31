d025fe0fdaffbebc5ae2514f6208408dd5d39ee3
tusharshaha <tusharshaha128@gmail.com>
2023-07-31 22:35:53 +0600

    updated slider

--- frontend/components/Home/Companies.tsx
+++ frontend/components/Home/Companies.tsx
@@ -6,13 +6,13 @@ import SwiperCore, { Pagination, Navigation, Autoplay } from 'swiper';
 SwiperCore.use([Pagination, Navigation, Autoplay]);
 
 const Companies: React.FC = () => {
-  const logos = new Array(6).fill(0)
+  const logos = new Array(7).fill(0)
 
   return (
     <div className='cus-container my-20 py-20 border-y'>
       <Swiper
-        slidesPerView={5}
-        spaceBetween={20}
+        slidesPerView={6}
+        spaceBetween={120}
         slidesPerGroup={1}
         loop={true}
         autoplay={{
@@ -23,9 +23,7 @@ const Companies: React.FC = () => {
         className="mySwiper">
         {
           logos.map((ele, i) => <SwiperSlide key={i}>
-            <div className='flex items-center justify-center group'>
-              <Image className='w-[90px] h-[90px] grayscale group-hover:grayscale-0 transition duration-500' src={`/logos/${i + 1}.webp`} height={100} width={100} alt='' />
-            </div>
+            <Image className='w-[90px] h-[90px] grayscale hover:grayscale-0 transition duration-500' src={`/logos/${i + 1}.webp`} height={100} width={100} alt='' />
           </SwiperSlide>)
         }
       </Swiper>
