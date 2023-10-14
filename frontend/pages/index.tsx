2bf9d275c830e2d003a1aba7ffa9beb325cc2360
tusharshaha <tusharshaha128@gmail.com>
2023-10-14 21:15:32 +0600

    updated hero section and navbar style

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,7 +5,6 @@ import JobCirculars from "@/components/Home/JobCirculars";
 import HowItWork from "@/components/Home/HowItWork";
 import DownloadApp from "@/components/Home/DownloadApp";
 import BestCandidate from "@/components/Home/BestCandidate";
-import Companies from "@/components/Home/Companies";
 
 const Home: React.FC = () => {
   return (
@@ -15,7 +14,6 @@ const Home: React.FC = () => {
       <HowItWork></HowItWork>
       <DownloadApp></DownloadApp>
       <BestCandidate></BestCandidate>
-      <Companies></Companies>
     </Layout>
   );
 };
