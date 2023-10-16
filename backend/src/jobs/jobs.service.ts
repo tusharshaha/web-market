3382034c06a35dab23960e59231d056202d9154b
tusharshaha <tusharshaha128@gmail.com>
2023-10-16 22:57:10 +0600

    updated limit and offset logic

--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 @Injectable()
 export class JobsService {
   async getJobList({ limit, offset }) {
-    if (limit > 20) {
+    if (!Number(limit) || !Number(offset) || limit > 20) {
       return;
     }
     const res = await axios.get(`${process.env.JOB_API_URL}/api`, {
