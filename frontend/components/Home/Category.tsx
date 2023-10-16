fe06f8d100fb4f5da4ae091359f4d95e779aefa9
tusharshaha <tusharshaha128@gmail.com>
2023-10-16 22:58:06 +0600

    created serverside rendering on job page

--- frontend/components/Home/Category.tsx
+++ frontend/components/Home/Category.tsx
@@ -9,7 +9,7 @@ const Category: React.FC = () => {
             <SectionTitle title='Popular Category' subTitle='Many desktop publishing packages and web page editors' />
             <div className='grid grid-cols-2 md:grid-cols-4 gap-3 mt-10'>
                 {
-                    categories.map((ele, i) => <Link key={i} className='rounded-lg bg-gray-200 p-5 text-center hover:bg-primary hover:text-white transition-all duration-300' href="">Accounting/Finance (305)</Link>)
+                    categories.map((ele, i) => <Link key={i} className='rounded-lg bg-gray-200 p-5 text-center hover:bg-primary hover:text-white trans' href="">Accounting/Finance (305)</Link>)
                 }
             </div>
 
--- frontend/pages/jobs.tsx
+++ frontend/pages/jobs.tsx
@@ -1,12 +1,42 @@
+import { NextPage } from 'next';
 import Layout from '@/components/Layout';
-import React from 'react';
+import { QueryClient, useQuery, dehydrate } from "react-query";
+import axiosRequest from '@/utils/axios.service';
 
-const Jobs: React.FC = () => {
+const getJobList = async (offset: string) => {
+  const res = await axiosRequest.get("/api/jobs", {
+    params: {
+      limit: 20,
+      offset
+    }
+  });
+  return res
+}
+
+const Jobs: NextPage = () => {
+  const [offset, setOffset] = useState(string)
+  useQuery('jobs', ())
   return (
-    <Layout>
-      
+    <Layout
+      title='Web Market | Jobs'
+      description='get you desire remote jobs'
+      keywords='jobs, remote jobs'
+    >
+
     </Layout>
   );
 };
+
+export async function getServerSideProps(context: { query: { offset: string } }) {
+  const { offset } = context.query;
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery(["jobs", offset], () => getJobList(offset))
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient)
+    },
+  };
+}
 
 export default Jobs;
