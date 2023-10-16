05a6f282cf9ebe62a7fbf48bb325d38973ed4c0d
tusharshaha <tusharshaha128@gmail.com>
2023-10-16 21:13:27 +0600

    updated the job backend logic

--- frontend/utils/axios.service.ts
+++ frontend/utils/axios.service.ts
@@ -8,6 +8,12 @@ const axiosConfig: AxiosRequestConfig = {
 
 const axiosRequest: AxiosInstance = axios.create(axiosConfig);
 
-
+axiosRequest.interceptors.response.use(
+  (res) => res.data,
+  (err) => {
+    // Handle errors globally here if necessary
+    return Promise.reject(err.message);
+  },
+);
 
 export default axiosRequest;
