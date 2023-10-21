c2f9e1b0f5411320c76af057739b3167714e31ce
tusharshaha <tusharshaha128@gmail.com>
2023-10-21 21:29:54 +0600

    added password hide and view option

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -1,15 +1,18 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { FaBackwardStep } from 'react-icons/fa6';
+import { useState } from 'react';
+import { FaBackwardStep, FaEye, FaEyeSlash } from 'react-icons/fa6';
 
 const Register: NextPage = () => {
+  const [showPass, setShowPass] = useState(false);
   const router = useRouter();
   const handleBack = () => router.back();
+  const handShowPass = ()=> setShowPass(!showPass);
   return (
     <div className="register flex items-start justify-center px-10">
       <div className='max-w-3xl'>
         <button onClick={handleBack} className="btn glass btn-sm sm:btn-md flex items-center gap-1 text-white mb-5">
-          <span><FaBackwardStep/></span>
+          <span><FaBackwardStep /></span>
           Back
         </button>
         <div className="rounded-md shadow-2xl overflow-hidden">
@@ -27,11 +30,16 @@ const Register: NextPage = () => {
                 className="w-full border-2 border-primary focus:outline-none focus:caret-primary rounded-full px-6 py-2"
                 placeholder='Your email'
               />
-              <input
-                type="password"
-                className='w-full border-2 border-primary focus:outline-none focus:caret-primary rounded-full px-6 py-2'
-                placeholder='Your password'
-              />
+              <div className='w-full relative'>
+                <input
+                  type={showPass ? "text" : "password"}
+                  className='w-full border-2 border-primary focus:outline-none focus:caret-primary rounded-full px-6 py-2'
+                  placeholder='Your password'
+                />
+                <button onClick={handShowPass} className='absolute top-[13px] right-[20px] text-slate-400'>
+                  {showPass ? <FaEye /> : <FaEyeSlash />}
+                </button>
+              </div>
               <div className='flex justify-end w-full'>
                 <button className='trans hover:text-primary text-sm md:text-base'>Forgot Password?</button>
               </div>
