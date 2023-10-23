7442bea2b12a16d9d65c4c4a02217f4112986860
tusharshaha <tusharshaha128@gmail.com>
2023-10-23 13:10:13 +0600

    updated confirm main temp path

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -8,7 +8,7 @@ import { User } from "./schemas/user.schema";
 import * as bcrypt from "bcryptjs";
 import * as crypto from "crypto";
 import { Model } from "mongoose";
-import confirmMailTemp from "@/utils/confirm.temp";
+import confirmMailTemp from "../utils/confirm.temp";
 import { JwtService } from "@nestjs/jwt";
 import { SignUpDto } from "./dto/signup.dto";
 import { LoginDto } from "./dto/login.dto";
