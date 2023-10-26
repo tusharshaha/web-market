eae98c90b51d6fb0a55167ef7ebef13178d8d0c2
tusharshaha <tusharshaha128@gmail.com>
2023-10-26 20:37:24 +0600

    updated blog responsive style

--- frontend/pages/blogs/index.tsx
+++ frontend/pages/blogs/index.tsx
@@ -11,7 +11,7 @@ const Blogs: NextPage = () => {
       description=""
     >
       <div className="cus-container my-14">
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {
             candidates.map((ele, i)=> <BlogCard key={i}/>)
           }
