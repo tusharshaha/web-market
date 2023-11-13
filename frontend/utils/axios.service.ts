b6f904b3fe4fa84f40d6f25355388239838d20b6
tusharshaha <tusharshaha128@gmail.com>
2023-11-13 12:04:02 +0600

    updated private and public api instance

--- frontend/utils/axios.service.ts
+++ frontend/utils/axios.service.ts
@@ -3,12 +3,12 @@ import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 const axiosConfig: AxiosRequestConfig = {
   baseURL: process.env.NEXT_PUBLIC_API,
   timeout: 10000,
-  withCredentials: true
 };
 
-const axiosRequest: AxiosInstance = axios.create(axiosConfig);
+const publicApi: AxiosInstance = axios.create(axiosConfig);
+const privateApi: AxiosInstance = axios.create({ ...axiosConfig, withCredentials: true });
 
-axiosRequest.interceptors.response.use(
+publicApi.interceptors.response.use(
   (res) => res.data,
   (err) => {
     // Handle errors globally here if necessary
@@ -16,4 +16,17 @@ axiosRequest.interceptors.response.use(
   },
 );
 
-export default axiosRequest;
+privateApi.interceptors.response.use(
+  (res) => res.data,
+  async (err) => {
+    const prevRequest = err.config;
+    if (err?.response?.status === 403 && !prevRequest?.sent) {
+      prevRequest.sent = true;
+      await publicApi.get("/auth/refresh", { withCredentials: true });
+      return privateApi(prevRequest);
+    }
+    return Promise.reject(err.message);
+  }
+);
+
+export { publicApi, privateApi };
