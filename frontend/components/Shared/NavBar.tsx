5841acce6d3322182d4195aa9fa8e7c81dc2b34d
tusharshaha <tusharshaha128@gmail.com>
2023-11-21 20:26:25 +0600

    updated menu style

--- frontend/components/Shared/NavBar.tsx
+++ frontend/components/Shared/NavBar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiMenu } from 'react-icons/fi';
 import { FaAngleLeft, FaRegEye } from 'react-icons/fa';
 import { AiOutlinePlus } from "react-icons/ai";
@@ -11,7 +11,7 @@ import useAuth from "@/hooks/useAuth";
 const NavBar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [menu, setMenu] = useState(false);
-    const { email, userImage } = useAuth();
+    const { email, userImage, isLoading, logout } = useAuth();
     const router = useRouter();
 
     const handleToggle = () => {
@@ -26,6 +26,10 @@ const NavBar: React.FC = () => {
         router.push("/register");
     }
 
+    const handleLogout = () => {
+        logout();
+        setMenu(false);
+    }
     const navItem = <>
         <li onClick={handleToggle} className="bg-primary p-2 text-white flex md:hidden items-center justify-between">
             <span>Menu</span>
@@ -56,9 +60,11 @@ const NavBar: React.FC = () => {
                         </ul>
                     </div>
                     <div className="flex justify-center items-center">
-                        {!email ?
+                        
+                        {!email && isLoading &&
                             <button onClick={handleRegister} className="web-btn2 tracking-widest"> <AiOutlinePlus className="font-bold mr-2" /> Join</button>
-                            :
+                        }
+                        {email && !isLoading &&
                             <button onClick={handleMenu} className="avatar">
                                 <div className="w-[30px] rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                                     {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -67,18 +73,21 @@ const NavBar: React.FC = () => {
                             </button>
                         }
                         {/* tooltip menu  */}
-                        <div className={`${menu ? "" : "hidden"} absolute top-[83px] right-[10px] bg-white text-black rounded-md`}>
+                        <div className={`${menu ? "" : "hidden"} absolute top-[70px] right-[11px] bg-white text-black rounded-md py-1`}>
+                        <div className="relative">
+                        <div className="w-[10px] h-[10px] bg-white absolute top-[-9px] right-[15px] rotate-45"></div>
+                        </div>
                             <ul className="w-[170px]">
                                 {
                                     menuItems.map((ele, i) => (
-                                        <li key={i} className="border-y py-1 px-2 flex items-center gap-2">
+                                        <li key={i} className="border-b py-1 px-2 flex items-center gap-2">
                                             <span className="text-primary">{ele.icon}</span>
                                             <Link href={ele.path} className="hover:text-primary">{ele.title}</Link>
                                         </li>
                                     ))
                                 }
-                                <li className="border-y py-1 px-2">
-                                    <button className="flex items-center gap-2 hover:text-primary">
+                                <li className="py-1 px-2">
+                                    <button onClick={handleLogout} className="flex items-center gap-2 hover:text-primary">
                                         <span className="text-primary"><MdLogout /></span>
                                         Log Out
                                     </button>
--- frontend/hooks/useAuth.ts
+++ frontend/hooks/useAuth.ts
@@ -5,18 +5,35 @@ import { useQuery } from 'react-query';
 
 interface Auth extends User {
   isLoading: boolean,
+  logout: () => void
 }
+
 const getProfile = async (): Promise<User> => {
   const res: User = await privateApi.get("/auth/profile");
   return res
 }
+
 const useAuth = (): Auth => {
+  const [user, setUser] = useState<User | {}>({});
   const { data, isLoading } = useQuery("profile", getProfile,
     {
       staleTime: 60000,
-      cacheTime: 60000
+      cacheTime: 60000,
+      retry: 1
     })
-  return { ...data, isLoading } as Auth;
+  useEffect(() => {
+    setUser(data as User);
+  }, [data])
+
+  const logout = async () => {
+    await privateApi("/auth/logout")
+    setUser({});
+  }
+  return {
+    ...user,
+    isLoading,
+    logout
+  } as Auth;
 };
 
 export default useAuth;
