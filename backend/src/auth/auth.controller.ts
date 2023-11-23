f73c63abcceee60ce46c4b0031070328853cce74
tusharshaha <tusharshaha128@gmail.com>
2023-11-23 21:56:53 +0600

    updated useAuth hook

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -75,8 +75,8 @@ export class AuthController {
     try {
       const { userId } = req.user;
       await this.authService.logout(userId);
-      res.clearCookie("access_token");
-      res.clearCookie("refresh_token");
+      res.clearCookie("access_token", { path: "/" });
+      res.clearCookie("refresh_token", { path: "/" });
       res.json({ message: "Successfully logout" });
     } catch (error) {
       return handleError(error);
