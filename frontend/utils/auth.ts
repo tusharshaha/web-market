e53715041a2459663357abb49dabc9477c9c47ac
tusharshaha <tusharshaha128@gmail.com>
2023-12-02 22:38:29 +0600

    updated middleware and created helper function

---
+++ frontend/utils/auth.ts
@@ -0,0 +1,49 @@
+import { NextRequest } from "next/server";
+
+const commonOptions: RequestInit = {
+  mode: "cors",
+  credentials: "include",
+};
+
+const makeRequest = (url: string, options: RequestInit) =>
+  fetch(`${process.env.NEXT_PUBLIC_API}${url}`, {
+    ...commonOptions,
+    ...options,
+  }).then((res) => res.json());
+
+export async function getUser(req: NextRequest) {
+  const access_token = req.cookies.get("access_token")?.value || "";
+  const refresh_token = req.cookies.get("refresh_token")?.value || "";
+
+  const profileData = await makeRequest("/auth/profile", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+  if (profileData.statusCode === 401) {
+    const refreshData = await makeRequest("/auth/refresh", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${refresh_token}`,
+      },
+    });
+
+    if (refreshData.statusCode !== 401) {
+      const refreshedProfileData = await makeRequest("/auth/profile", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+      return refreshedProfileData;
+    } else {
+      return {};
+    }
+  }else{
+    return profileData
+  }
+}
