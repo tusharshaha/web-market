d2e74e563c89725c9ddf19374bac8ba6bb2caf77
tusharshaha <tusharshaha128@gmail.com>
2023-12-06 20:05:36 +0600

    removed get user function from middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,41 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getUser } from "./utils/auth";
-import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
-
-const cookieOption: Partial<ResponseCookie> = {
-  httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  path: "/",
-};
 
 export default async function middleware(req: NextRequest) {
-  const response = NextResponse.next();
-  const user = await getUser(req);
   const pathname = req.nextUrl.pathname;
-  response.cookies.set("user", user.email, {
-    ...cookieOption,
-    maxAge: 60 * 60 * 1000,
-  });
-  return response;
-  // if (pathname.startsWith("/register") && user.email) {
-  //   return NextResponse.redirect(new URL("/", req.url));
-  // }
-  // if (pathname.startsWith("/dashboard") && !user.email) {
-  //   return NextResponse.redirect(new URL("/", req.url));
-  // }
-  // // set token to the browser
-  // if (user?.token?.access_token) {
-  //   response.cookies.set("access_token", user.token.access_token, {
-  //     ...cookieOption,
-  //     maxAge: 60 * 60 * 1000,
-  //   });
-  //   response.cookies.set("refresh_token", user.token.refresh_token, {
-  //     ...cookieOption,
-  //     maxAge: 60 * 60 * 24 * 7 * 1000,
-  //   });
-  //   return response;
-  // }
+  const refresh_token = req.cookies.get("refresh_token")?.value || "";
+
+  if (pathname.startsWith("/register") && refresh_token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (pathname.startsWith("/dashboard") && !refresh_token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 }
 
 export const config = {
