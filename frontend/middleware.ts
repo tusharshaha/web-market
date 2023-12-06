edc7695cca17fa93bf1c3fca4616e1b985a3328c
tusharshaha <tusharshaha128@gmail.com>
2023-12-06 13:54:39 +0600

    updated middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -20,17 +20,17 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/", req.url));
   }
   // set token to the browser 
-  if (user?.token?.access_token) {
-    response.cookies.set("access_token", user.token.access_token, {
-      ...cookieOption,
-      maxAge: 60 * 60 * 1000,
-    });
-    response.cookies.set("refresh_token", user.token.refresh_token, {
-      ...cookieOption,
-      maxAge: 60 * 60 * 24 * 7 * 1000,
-    });
-    return response;
-  }
+  // if (user?.token?.access_token) {
+  //   response.cookies.set("access_token", user.token.access_token, {
+  //     ...cookieOption,
+  //     maxAge: 60 * 60 * 1000,
+  //   });
+  //   response.cookies.set("refresh_token", user.token.refresh_token, {
+  //     ...cookieOption,
+  //     maxAge: 60 * 60 * 24 * 7 * 1000,
+  //   });
+  //   return response;
+  // }
 }
 
 export const config = {
