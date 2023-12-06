fbcaa44a9c2abc4307071c9ed02bdc0ae6c02535
tusharshaha <tusharshaha128@gmail.com>
2023-12-06 20:13:58 +0600

    testing middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,15 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getUser } from "./utils/auth";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+const cookieOption: Partial<ResponseCookie> = {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  path: "/",
+};
 
 export default async function middleware(req: NextRequest) {
+  const response = NextResponse.next();
   const pathname = req.nextUrl.pathname;
   const refresh_token = req.cookies.get("refresh_token")?.value || "";
-
-  if (pathname.startsWith("/register") && refresh_token) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-  if (pathname.startsWith("/dashboard") && !refresh_token) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
+  response.cookies.set("token", refresh_token, cookieOption)
+  return response
+  // if (pathname.startsWith("/register") && refresh_token) {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
+  // if (pathname.startsWith("/dashboard") && !refresh_token) {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
 }
 
 export const config = {
